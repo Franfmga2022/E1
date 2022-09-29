cdadf6429315c8d2aecd2c06889ee874171f7f3d
Franfmga2022 <francisco.garcia.alcivar@utelvt.edu.ec>
2022-09-29 15:42:08 -0500

    Agregue el programa de velocimetro

---
+++ compara.cpp
@@ -0,0 +1,33 @@
+// creador: Francisco Garcia
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y;
+        cout<<"Ingrese x= "; cin>>x;
+        cout<<"Ingrese y= "; cin>>y;
+
+	if(x==y){
+	cout<<"El valor de x= "<<x<<" es igual a y= "<<y<<endl;
+	
+
+
+	}else{
+
+	 if (x<y){
+	  cout<<"El valor de x= "<<x<<" es menor a y= "<<y<<endl;
+
+
+
+	   }else{
+	
+	     cout<<"El valor de y= "<<y<<" es menor a x= "<<x<<endl;
+
+	}
+
+  }
+	
+return 0;
+
+}
+
